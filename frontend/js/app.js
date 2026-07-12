d17d3a1f2fe47aaa9ead2cca5f2436325e340558
Sweni01 <patelarchi967@gmail.com>
2026-07-12 21:12:51 +0530

    Update frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,4 @@
-const API_BASE =
-window.location.hostname==="127.0.0.1" ||
-window.location.hostname==="localhost"
-?
-"http://127.0.0.1:8000"
-:
-"https://cravewise-backend.onrender.com";
+const API_BASE = "https://cravewise-backend.onrender.com";
 
 loadConditions();
 
